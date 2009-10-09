eff380afcf66bd0b62312027e5ff747010cd48cb
Aaron Quint <aaron@quirkey.com>
2009-10-09 13:07:31 -0400

    Added simple logging route and made sure the app is running

--- javascripts/json_store.js
+++ javascripts/json_store.js
@@ -1,7 +1,16 @@
 (function($) {
   
-  $.sammy(function() {
-    this.element_selector = '#main';
+  var app = $.sammy(function() {
+    this.element_selector = '#main';    
+
+    this.get('#/', function(context) {
+      context.log('Yo yo yo');
+    });
+    
+  });
+  
+  $(function() {
+    app.run('#/');
   });
   
 })(jQuery);

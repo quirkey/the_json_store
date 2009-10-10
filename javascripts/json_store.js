949646113f3ea9c5524a515f543bb36e3cf17ca2
Aaron Quint <aaron@quirkey.com>
2009-10-10 16:09:55 -0400

    Refactored the item loading code into a before block

--- javascripts/json_store.js
+++ javascripts/json_store.js
@@ -4,18 +4,29 @@
     this.element_selector = '#main';    
     this.use(Sammy.Template);
 
-    this.get('#/', function(context) {
+    this.before(function() {
+      // load the items
+      var context = this;
       $.ajax({
         url: 'data/items.js', 
         dataType: 'json',
+        async: false,
         success: function(items) {
-          $.each(items, function(i, item) {
-            context.partial('templates/item.template', {item: item}, function(rendered) {
-              context.$element().append(rendered);
-            });
-          });
+          context.items = items;
         }
       });
+    });
+
+    this.get('#/', function(context) {
+      $.each(context.items, function(i, item) {
+        context.partial('templates/item.template', {item: item}, function(rendered) {
+          context.$element().append(rendered);
+        });
+      });
+    });
+    
+    this.get('#/item/:id', function(context) {
+      
     });
     
   });

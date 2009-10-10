db01ab3dab8833753a539487761d2d21b58744fa
Aaron Quint <aaron@quirkey.com>
2009-10-10 17:23:48 -0400

    Fixed json syntax error

--- data/items.js
+++ data/items.js
@@ -5,7 +5,7 @@
     "image": "http://ecx.images-amazon.com/images/I/51og8BkN8jL._SS250_.jpg",
     "large_image": "http://ecx.images-amazon.com/images/I/51og8BkN8jL._SS500_.jpg",
     "price": 9.98,
-    "url": "http://www.amazon.com/Door-Religious-Knives/dp/B001FGW0UQ/?tag=quirkey-20",
+    "url": "http://www.amazon.com/Door-Religious-Knives/dp/B001FGW0UQ/?tag=quirkey-20"
   },
   {
     "title": "Album",
